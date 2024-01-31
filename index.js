d4a653aa4c9c17780111f7a9086302f36ed10489
majidgeeks <majidrajput246@gmail.com>
2024-01-31 16:29:29 +0500

    some changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors'
 
 
 const app = express();
@@ -19,6 +20,7 @@ const users = [
 
 
 app.use(express.json());
+// app.use(cors());
 
 app.get('/users',(req,res) => {
    res.send(users)
@@ -31,10 +33,28 @@ app.post('/user', (req, res) => {
 });
 
 
+app.put('/user/:id',(req,res) => {
+    let index = users.findIndex(v => v.id === Number(req.params.id));
+    if(index !== -1){
+
+        users.splice(index, 1, {id: Number(req.params.id), ...req.body});
+    }
+    res.send({message: "user updated successfully"});
+})
+
+// place order from ecommerce
+app.post("/order",(req,res) => {
+    console.log("order details", req.body);
+    res.send({message: "order placed"})
+})
+
 app.delete('/user/:id', (req, res) => {
  
     let index = users.findIndex(v => v.id === Number(req.params.id));
-    users.splice(index, 1);
+    if(index !== -1){
+
+        users.splice(index, 1);
+    }
     res.send("user deleted successfully");
 })
 
